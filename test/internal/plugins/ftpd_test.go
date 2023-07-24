ab5586c08b80513d510488ca2e9d81f8da088c4a
eltsai <eltsai@umich.edu>
2023-07-24 00:36:53 -0400

    Added more ftp test cases

--- test/internal/plugins/ftpd_test.go
+++ test/internal/plugins/ftpd_test.go
@@ -10,6 +10,7 @@ import (
 
 func TestFTPClient(t *testing.T) {
 	// Connect to the FTP server
+	// Change port to the port running ftp server
 	conn, err := net.Dial("tcp", "localhost:2121")
 	if err != nil {
 		t.Fatalf("Failed to connect to FTP server: %s", err)
@@ -90,6 +91,65 @@ func TestFTPClient(t *testing.T) {
 		// Check if the response equals "215 UNIX Type: L8."
 		if response != "215 UNIX Type: L8.\r\n" {
 			t.Fatalf("Invalid SYST command response: %s", response)
+		}
+	})
+
+	t.Run("MakeDirectory", func(t *testing.T) {
+		fmt.Println("Testing Make Directory Command")
+
+		// Send the MKD command to create the directory "test1"
+		cmd := "MKD test1\r\n"
+		_, err = conn.Write([]byte(cmd))
+		if err != nil {
+			t.Fatalf("Failed to send MKD command: %s", err)
+		}
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			t.Fatalf("Failed to read MKD command response: %s", err)
+		}
+
+		// Check if the response starts with "257 " which indicates success
+		if !strings.HasPrefix(response, "257 ") {
+			t.Fatalf("Failed to create directory: %s", response)
+		}
+	})
+
+	t.Run("ChangeDirectory", func(t *testing.T) {
+		fmt.Println("Testing Change Directory Command")
+
+		// Send the CWD command to change to the "test1" directory
+		cmd := "CWD test1\r\n"
+		_, err = conn.Write([]byte(cmd))
+		if err != nil {
+			t.Fatalf("Failed to send CWD command: %s", err)
+		}
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			t.Fatalf("Failed to read CWD command response: %s", err)
+		}
+
+		// Check if the response starts with "250 " which indicates success
+		if !strings.HasPrefix(response, "250 ") {
+			t.Fatalf("Failed to change directory: %s", response)
+		}
+
+		// Send the CWD command to change to the "/home" directory
+		cmd = "CWD /home\r\n"
+		_, err = conn.Write([]byte(cmd))
+		if err != nil {
+			t.Fatalf("Failed to send CWD command: %s", err)
+		}
+
+		response, err = reader.ReadString('\n')
+		if err != nil {
+			t.Fatalf("Failed to read CWD command response: %s", err)
+		}
+
+		// Check if the response starts with "250 " which indicates success
+		if !strings.HasPrefix(response, "250 ") {
+			t.Fatalf("Failed to change directory: %s", response)
 		}
 	})
 }
